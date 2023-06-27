680f260e4d753cfb78e14b25dabf2cabb8b142c7
Archita Srinivasan <archiesrinivasan0728@gmail.com>
2023-06-27 12:45:41 -0400

    get started

--- explorio/login page.swift
+++ explorio/login page.swift
@@ -9,27 +9,70 @@ import SwiftUI
 struct login_page: View {
     var body: some View {
         @State var showDetails = true
-            ZStack(alignment: .topLeading){
-                VStack (alignment: .leading){
-                    Button("<-- Back"){
+
+        
+           
+                ZStack(alignment: .topLeading){
+                    VStack (alignment: .leading){
+                      
+                        VStack{
+                            Image("star")
+                            Button("Login"){
+                                
+                            }//end of login button
+                            
+                            Button("Sign Up"){
+                                }
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                        }
                         
                     }
-                    VStack{
-                        Image("star")
-                        Button("Login"){
-                            
-                        }//end of login button
-                        
-                        Button("Sign Up"){
-                            
-                        }//end of sign up button
-                        
-                        
-                    }//end of vstack
-                }//end of vstack
-            }//end of zstack
-        }
-    }
+                    }
+                }
+            }
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct login_page_Previews: PreviewProvider {
     static var previews: some View {
