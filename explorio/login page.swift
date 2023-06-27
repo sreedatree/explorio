4a373f6d79105618c4436ef24681fbb5fcbb0cb8
Archita Srinivasan <archiesrinivasan0728@gmail.com>
2023-06-27 15:23:12 -0400

    design

--- explorio/login page.swift
+++ explorio/login page.swift
@@ -10,20 +10,36 @@ struct login_page: View {
     var body: some View {
         @State var showDetails = true
         NavigationStack{
-                ZStack(alignment: .topLeading){
-                    VStack (alignment: .leading){
+            ZStack(alignment: .topLeading){
+                VStack (alignment: .leading){
                       
-                        VStack{
-                            Image("star")
-                            Button("Login"){
-                                
-                            }//end of login button
-                            
-                            Button("Sign Up"){
-                            }
-                            NavigationLink(destination: homePage()) {
-                                Text("Continue as Guest")}
-                        }
+                    VStack(){
+                    Image("star")
+                        .padding(.bottom, 75.0)
+                    Button("Login"){
+                    }//end of login button
+                    .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(10)
+                    .background(Color.gray)
+                    .foregroundColor(.white)
+                    
+                    
+                    Button("Sign Up"){
+                        }//end of sign up button
+                    .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(10)
+                    .background(Color.gray)
+                    .foregroundColor(.white)
+                        
+                        
+                        
+        NavigationLink(destination: homePage()) {
+            Text("Continue as Guest")}
+        .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+        .cornerRadius(10)
+        .background(Color.gray)
+        .foregroundColor(.white)
+    }
             
                             
                             
