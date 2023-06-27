81dfabe00c1780bd132f1e09c880592621d7bdac
Archita Srinivasan <archiesrinivasan0728@gmail.com>
2023-06-27 11:13:46 -0400

    hi guys what's up

---
+++ explorio/login page.swift
@@ -0,0 +1,20 @@
+//
+//  login page.swift
+//  explorio
+//
+//  Created by scholar on 6/27/23.
+//
+
+import SwiftUI
+
+struct login_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct login_page_Previews: PreviewProvider {
+    static var previews: some View {
+        login_page()
+    }
+}
