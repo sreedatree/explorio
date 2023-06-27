d240c8ffac1199555d5ae11ad669db6ff0810e3b
Nhi Le <michellelele156@gmail.com>
2023-06-27 11:09:32 -0400

    homepage-nhi

---
+++ explorio/homePage.swift
@@ -0,0 +1,27 @@
+//
+//  homePage.swift
+//  explorio
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+struct homePage: View {
+    var body: some View {
+        VStack {
+            Image("earth")
+                .resizable()
+                .frame(width: 200.0, height: 200.0)
+            Button("poop") {
+                
+            }
+        }
+    }
+}
+
+struct homePage_Previews: PreviewProvider {
+    static var previews: some View {
+        homePage()
+    }
+}
